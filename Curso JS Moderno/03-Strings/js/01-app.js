f546a99087cc88284bfcd68ef7068d7e92122f2e
Jhon Chavez <jhchg7@gmail.com>
2020-09-03 22:51:35 -0400

    crear strings con JS

---
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,11 @@
+//Los strings o cadenas de texto representan un texto que se puede asignar a una variable
+//no se deben ligar las comillas
+
+const producto = "Monitor de 20 pulgadas"; //forma 1
+const producto2 = String('Monitor de 24 pulgadas'); //forma 2
+
+const producto3 = new String("Monitor de 27 pulgadas"); //forma 3
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
