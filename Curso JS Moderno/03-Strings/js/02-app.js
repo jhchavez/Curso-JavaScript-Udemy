c5858e92ac0143ddc3fa07a338862d091558bd56
Jhon Chavez <jhchg7@gmail.com>
2020-09-04 20:59:43 -0400

    String Methods-includes y length

---
+++ Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,13 @@
+const producto = 'Monitor de 20 pulgadas';
+
+console.log(producto);
+
+//conocer la cantidad de letras de la cadena de texto
+console.log(producto.length); //.length no utiliza parentesis
+
+
+// console.log(producto.indexOf('tablet'));
+
+console.log(producto.includes('tablet'));
+console.log(producto.includes('Monitor'));
+console.log(producto.includes('monitor'));
