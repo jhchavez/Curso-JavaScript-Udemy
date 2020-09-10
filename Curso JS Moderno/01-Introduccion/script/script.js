23f2511ccbb9642c89ca115dc4c3c7ca9734d250
Jhon Chavez <jhchg7@gmail.com>
2020-09-09 22:30:00 -0400

    eslint

--- Curso JS Moderno/01-Introduccion/script/script.js
+++ Curso JS Moderno/01-Introduccion/script/script.js
@@ -5,6 +5,6 @@ const nombre =  prompt('Cual es tu nombre?');
 document.querySelector('.contenido').innerHTML = `${nombre} Esta aprendiendo js moderno`;
 
 //asigna ek valor hacia la variable producto
-const nombre = 'Monitor 24 pulgadas'
+const nombre = 'Monitor 24 pulgadas';
 
 /*Bloque grande de codigo */
