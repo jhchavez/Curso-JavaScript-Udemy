7bec35a421d2daf944d93f94fda7cb72a3ad1731
Jhon Chavez <jhchg7@gmail.com>
2020-09-10 22:48:54 -0400

    Objeto Math

---
+++ Curso JS Moderno/04-Numeros/js/03-app.js
@@ -0,0 +1,41 @@
+let resultado;
+
+//PI
+resultado = Math.PI;
+
+//redondear
+resultado = Math.round(2.8);
+resultado = Math.round(2.2);
+resultado = Math.round(2.6);
+resultado = Math.round(2.4);
+resultado = Math.round(2.5);
+
+//redondear hacia arriba 
+resultado = Math.ceil(2.1);
+
+//redondear hacia abajo
+resultado = Math.floor(2.8);
+
+//Raiz cuadrada
+resultado = Math.sqrt(144);
+
+//valor absoluto
+resultado = Math.abs(-500);
+
+//potencia
+resultado = Math.pow(8, 3);
+resultado = Math.pow(2, 4);
+
+//Minimo
+resultado = Math.min(3, 5, 1, 12, -3);
+
+//Maximo
+resultado = Math.max(3, 5, 1, 12, -3);
+
+//Aleatorio 
+resultado = Math.random() * 20; //no recomendado
+
+//Aleatorio dentro de un rango
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
