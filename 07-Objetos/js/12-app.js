b8d8633943267445b61a52cd2cafc4f918496585
Jhon Chavez <jhchg7@gmail.com>
2020-11-07 23:42:40 -0400

    Object Constructor

---
+++ 07-Objetos/js/12-app.js
@@ -0,0 +1,19 @@
+// Object Literal
+const producto = {
+    nombre: "Monitor 20 Pulgadas",
+    precio: 300, 
+    disponible: true,
+}
+
+//Object Constructor
+function Producto(nombre, precio){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponible = true;
+}
+
+const producto2 = new Producto('Monitor de 20 pulgadas', 500);
+console.log(producto2);
+
+const producto3 = new Producto('Telefono Samsung J9', 800);
+console.log(producto3);
