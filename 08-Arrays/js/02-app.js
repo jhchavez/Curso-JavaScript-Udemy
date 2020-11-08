989f4bb6490bbe6035250e96fc724c45cc8a833f
Jhon Chavez <jhchg7@gmail.com>
2020-11-08 00:37:32 -0400

    Acceder a los valores de un array

---
+++ 08-Arrays/js/02-app.js
@@ -0,0 +1,10 @@
+const numeros = [10, 20, 30, 40, 50, [1, 2, 3, 4, 5]];
+
+console.log(numeros);
+console.table(numeros);
+
+//acceder al array
+console.log(numeros[2]);
+console.log(numeros[0]);
+console.log(numeros[20]);
+console.log(numeros[5][2]);
