34c597b82d801f62e43f7a442eb569058cb29c02
Jhon Chavez <jhchg7@gmail.com>
2020-11-08 00:57:21 -0400

    Agregar nuevos valores a un array

---
+++ 08-Arrays/js/04-app.js
@@ -0,0 +1,8 @@
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+months[0] = 'new month';
+months[7] = 'last month';
+months[10] = 'last month';
+
+
+console.table(months);
