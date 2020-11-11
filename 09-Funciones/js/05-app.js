648bcfad64ea14af7064109b9039a5397e9c3b5a
Jhon Chavez <jhchg7@gmail.com>
2020-11-10 23:51:57 -0400

    Parametros y argumentos en funciones

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,15 @@
+function sumar(a, b){ //a y b son parametros.
+    console.log( a + b);
+}
+
+sumar(2, 3); //2 y 3 son argumentos.
+sumar(589, 389); 
+sumar(252, 302); 
+
+function saludar(nombre, apellido) {
+    console.log(`Hola ${nombre} ${apellido} `);
+}
+
+saludar('Juan', 'Chavez');
+saludar('Juan');
+saludar();
