c5a474bfe2e0afcf5a4afde48e8a279461cd5acb
Jhon Chavez <jhchg7@gmail.com>
2020-11-10 22:27:48 -0400

    Diferencia entre funcion y metodo

---
+++ 09-Funciones/js/04-app.js
@@ -0,0 +1,13 @@
+const number1 = 20;
+const number2 = "20";
+
+console.log( parseInt(number2) );// this is a function.
+
+console.log( number1.toString() );// this is a method.
+
+
+function sumar(){
+    console.log( 2 + 2);
+}
+
+sumar();
